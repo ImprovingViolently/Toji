97f6faa4a3ad80369178c61a9082bd3df32c24d5
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-27 01:42:21 +1100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+#DISCORD CONFIG
 token = 'your-token'
+
+#BOT CONFIG
+gifs = [
+    'https://tenor.com/view/toji-toji-jjk-fushiguro-jjk-toji-princess-toji-kawaii-gif-6512503698876272780',
+    'https://tenor.com/view/toji-jjk-jujutsu-kaisen-tuesday-toji-picmix-gif-8168723334892853958',
+    'https://tenor.com/view/toji-jjk-edit-toji-edit-toji-fushiguro-toji-jjk-gif-2595306297905933760',
+    'https://tenor.com/view/toji-toji-fushiguro-cute-wife-toji-wife-gif-13087645902577823041'
+]
