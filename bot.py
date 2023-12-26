b7feb2438a578aca525366a102aa0eff2a376a7e
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-27 02:31:17 +1100

    Continued refactroing, included comments.

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
+from config import token, gifs, blacklist, command
 import discord
-from cofig import token, gifs
-from random import choice
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,16 +13,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    
+    # Checks if author is itself in order to prevent a feedback loop.
     if message.author == client.user:
         return
+    
+    # Ensures it doesn't respond to a bot command by comparing to a list of blocked commands
+    if message.content.startswith(blacklist):
+        return
+    
+    # Test ping - Turn this into a switch statement or something similar for more commands. its easy trust
+    if message.content.startswith(command + 's' or 'status'):
+        await message.channel.send('I am alive (i didnt kms)')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+    # Checks all messages for 'toji', returns a random gif.
     msg = message.content.lower()
     word = 'toji'
     
     if word in msg:
-        await message.channel.send(choice(gifs))
+        await message.channel.send(random.choice(gifs))
 
 client.run(token)
