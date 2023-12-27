15ea66475e8597a9b6451dba56b81ae4e55b2115
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-27 13:18:18 +1100

    Added parser

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from config import token, gifs, blacklist, command
+from parse import msgParse
 import discord
 import random
 
@@ -13,24 +14,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    
+    msg = message.content.lower()
+
     # Checks if author is itself in order to prevent a feedback loop.
     if message.author == client.user:
-        return
-    
-    # Ensures it doesn't respond to a bot command by comparing to a list of blocked commands
-    if message.content.startswith(blacklist):
         return
     
     # Test ping - Turn this into a switch statement or something similar for more commands. its easy trust
     if message.content.startswith(command + 's' or 'status'):
         await message.channel.send('I am alive (i didnt kms)')
 
+    if message.content.startswith(command):
+        print(f'Parsed Message = ' + msgParse(msg))
+
     # Checks all messages for 'toji', returns a random gif.
-    msg = message.content.lower()
     word = 'toji'
     
     if word in msg:
-        await message.channel.send(random.choice(gifs))
+        if message.content.startswith(blacklist):
+            return
+        else:
+            await message.channel.send(random.choice(gifs))
 
 client.run(token)
