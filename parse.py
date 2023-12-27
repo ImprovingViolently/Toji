cdebdcdd1d69a457ddb35f0be9171f0ebbc7902d
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-27 16:36:31 +1100

    Added state checker

--- parse.py
+++ parse.py
@@ -4,3 +4,14 @@ def msgParse(message):
     if command in message:
         parsedMessage = message.replace(command, '')
         return parsedMessage
+    
+def tojiStateChecker(bool):
+    enabledStatement = 'armed and ready'
+    disabledStatement = 'disabled. Toji is dead for the third time'
+
+    if bool == 1:
+        return enabledStatement
+    elif bool == 0:
+        return disabledStatement
+    else:
+        return 
