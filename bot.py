ad5b19d7326ce16adc431f02addc01358d71cca0
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-27 15:45:18 +1100

    added toggle

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 from config import token, gifs, blacklist, command
-from parse import msgParse
+from parse import msgParse, tojiState
 import discord
 import random
 
 intents = discord.Intents.default()
 intents.message_content = True
+
+class toji:
+    alive = 1
 
 client = discord.Client(intents=intents)
 
@@ -19,13 +22,10 @@ async def on_message(message):
     # Checks if author is itself in order to prevent a feedback loop.
     if message.author == client.user:
         return
-    
-    # Test ping - Turn this into a switch statement or something similar for more commands. its easy trust
-    if message.content.startswith(command + 's' or 'status'):
-        await message.channel.send('I am alive (i didnt kms)')
 
     if message.content.startswith(command):
-        print(f'Parsed Message = ' + msgParse(msg))
+        parsedMessage = msgParse(msg)
+        await commandSwitch(parsedMessage, message)
 
     # Checks all messages for 'toji', returns a random gif.
     word = 'toji'
@@ -34,6 +34,57 @@ async def on_message(message):
         if message.content.startswith(blacklist):
             return
         else:
-            await message.channel.send(random.choice(gifs))
+            if toji.alive == 1:
+                await message.channel.send(random.choice(gifs))
+
+# Checks if it is a command
+
+async def commandSwitch(parsedMessage, message):
+    
+    # Toggle command turns on/off bot auto respond.
+    
+    if parsedMessage == 't' or parsedMessage == 'toggle':
+        if toji.alive == 1:
+            toji.alive = 0
+            embedVar = discord.Embed(title="Toji has been disabled!", description="To turn Toji back on, use " + command + "toggle", color=0x00ff00)
+            await message.channel.send(embed=embedVar)
+        elif toji.alive == 0:
+            toji.alive = 1
+            embedVar = discord.Embed(title="Toji has been enabled!", description="To turn Toji back off, use " + command + "toggle", color=0x00ff00)
+            await message.channel.send(embed=embedVar)
+        else:
+            embedVar = discord.Embed(title="ERROR", description="CONTACT MSG WITH ERROR code [TOJI_TOGGLE_CRIT_FAIL_ELIF]", color=0x00ff00)
+            await message.channel.send(embed=embedVar)
+    
+    # State checker. Returns an embed dependent on the toji.alive variable
+
+    elif parsedMessage == 's' or parsedMessage == 'status':
+        print(toji.alive)
+        if toji.alive == 0 or toji.alive == 1:    
+            embedVar = discord.Embed(title="TojiToggle", description="Toji is currently " + tojiState(toji.alive), color=0x00ff00)
+            embedVar.add_field(name= command + "toggle", value="To toggle Toji's auto respond.", inline=False)
+            await message.channel.send(embed=embedVar)
+        else:
+            embedVar = discord.Embed(title="ERROR", description="CONTACT MSG WITH ERROR code [TOJI_TOGGLE_CRIT_FAIL_ELIF]", color=0x00ff00)
+            await message.channel.send(embed=embedVar)  
+    
+    # Help command. Returns embed containing a list of commands
+
+    elif parsedMessage == 'h' or parsedMessage == 'help':
+        embedVar = discord.Embed(title="Help", description="Here is a list of commands:", color=0x00ff00)
+        embedVar.add_field(name= command + "help", value="Returns a list of commands.", inline=False)
+        embedVar.add_field(name= command + "status", value="Shows whether the bot is online.", inline=False)
+        await message.channel.send(embed=embedVar)
+    
+    #elif parsedMessage == 'debugoverridetojistate':
+    #    toji.alive = 123
+    #    await message.channel.send('MANUAL OVERRIDE ENABLED')
+    
+    # Fallback in case of invalid command
+    
+    else:
+        embedVar = discord.Embed(title="Invalid Command", description="You have sent an invalid command!", color=0x00ff00)
+        embedVar.add_field(name="For a list of commands, try:", value= command + "help", inline=False)
+        await message.channel.send(embed=embedVar)
 
 client.run(token)
