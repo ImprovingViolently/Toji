ae8adcd273c66e892dd67eee96c22270cabd8c8c
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-28 17:27:58 +1100

    Removed deprecated parser

--- parse.py
+++ parse.py
@@ -1,10 +1,3 @@
-from config import command
-
-def msgParse(message):
-    if command in message:
-        parsedMessage = message.replace(command, '')
-        return parsedMessage
-    
 def tojiStateChecker(bool):
     enabledStatement = 'armed and ready'
     disabledStatement = 'disabled. Toji is dead for the third time'
