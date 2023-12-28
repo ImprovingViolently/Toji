4c4d9256515e2446b4146a622545e5aba605582e
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-28 17:25:39 +1100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
-#DISCORD CONFIG
+import discord
+
+# DISCORD CREDS
 token = 'your-token'
 
-#BOT CONFIG
+# BOT CONFIG
+
+## Sets command prefix
+command = '^'
+
+## Sets discord bot's status.
+status = discord.Game(command+'help')
+
+## Pool of available gifs for the gif randomiser.
 gifs = [
     'https://tenor.com/view/toji-toji-jjk-fushiguro-jjk-toji-princess-toji-kawaii-gif-6512503698876272780',
     'https://tenor.com/view/toji-jjk-jujutsu-kaisen-tuesday-toji-picmix-gif-8168723334892853958',
@@ -9,8 +19,7 @@ gifs = [
     'https://tenor.com/view/toji-toji-fushiguro-cute-wife-toji-wife-gif-13087645902577823041'
 ]
 
-command = '^'
-
+## Does not respond with Toji gif if message starts with the following. Add any other comand prefixes you might have to this list.
 blacklist = (
     '!',
     '$',
