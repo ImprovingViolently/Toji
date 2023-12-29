f6165615a6d6fc59e833f5388a107839f8a64cb1
Silvery <99803467+ImprovingViolently@users.noreply.github.com>
2023-12-29 22:28:35 +1100

    Update config.py

--- config.py
+++ config.py
@@ -18,14 +18,6 @@ command = '^'
 ## Sets discord bot's status.
 status = discord.Game(command+'help')
 
-## Pool of available gifs for the gif randomiser.
-gifs = [
-    'https://tenor.com/view/toji-toji-jjk-fushiguro-jjk-toji-princess-toji-kawaii-gif-6512503698876272780',
-    'https://tenor.com/view/toji-jjk-jujutsu-kaisen-tuesday-toji-picmix-gif-8168723334892853958',
-    'https://tenor.com/view/toji-jjk-edit-toji-edit-toji-fushiguro-toji-jjk-gif-2595306297905933760',
-    'https://tenor.com/view/toji-toji-fushiguro-cute-wife-toji-wife-gif-13087645902577823041'
-]
-
 ## Does not respond with Toji gif if message starts with the following. Add any other comand prefixes you might have to this list.
 blacklist = (
     '!',
@@ -37,3 +29,13 @@ blacklist = (
     '%',
     '^'
 )
+
+## SQL injection blacklist
+sqlBlacklist = [
+    "'",
+    '"',
+    '%',
+    '$',
+    ' union ',
+
+]
